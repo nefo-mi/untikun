19d3f831de0ba1e77e4486cbfdaf937a7e903525
nefo_mi <nefo.mi@gmail.com>
2012-03-10 16:19:02 +0900

    データの数を増やして日本語に変更

--- app.rb
+++ app.rb
@@ -1,10 +1,27 @@
+# -*- encoding: utf-8 -*-
 require 'sinatra'
 require 'json'
 
 get '/' do
-    "Hello, world"
+  "それは、まさに、うんちく、だね？"
+end
+
+get '/untiku_test.json' do
+  JSON.generate({'untiku' => ['hoge', 'huga', 'hago']})
 end
 
 get '/untiku.json' do
-  JSON.generate({'untiku' => ['hoge', 'huga', 'hago']})
+  untiku = [
+    "便器に白が多いのは、どんな家にも合わせやすいから",
+    "日本人が１年間に使用するトイレットペーパーを地球に巻きつけると、地球１万周分になる",
+    "烏樞沙摩（うすさま）明王という神様の加護を得られると、他人にシモの世話にならないで済むらしい",
+    "国武将のトイレは、敵襲にあっても応戦ようだだっ広いものだったらしい。",
+    "猫の爪の雑菌がヤバい",
+    "うさぎは植物、昔は鳥扱い",
+    "日本でトイレを意味する言葉は、およそ５０種類以上ある",
+    "トイレットペーパーの質は国によって違う",
+    "うさぎはトイレの場所を覚える",
+    "初期のNASAでは、宇宙飛行士はおむつを履いていたらしい"
+  ]
+  JSON.generate({'untiku' => untiku})
 end
